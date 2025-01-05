52b824f41666edefa3cde173c54a5e0a6975d71b
sayful-hassan <sayf.fashion@gmail.com>
2025-01-05 19:35:25 +0530

    done with basics, needs to style & arrange specific answer to reach specific goal

--- js folder/index.js
+++ js folder/index.js
@@ -1,3 +1,4 @@
+//for page 1 q1
 document.addEventListener("DOMContentLoaded", ()=>{
   
   let radioButtons=document.querySelectorAll('input[name="q1"]')
@@ -10,6 +11,113 @@ radioButtons.forEach(radio => {
   });
 
 })
+
+//for page2 q2
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q2"]')
+let nextButton=document.getElementById("nextButton2")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+
+//for page3 q3
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q3"]')
+let nextButton=document.getElementById("nextButton3")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+
+
+//for page4 q4
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q4"]')
+let nextButton=document.getElementById("nextButton4")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+//for page5 q5
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q5"]')
+let nextButton=document.getElementById("nextButton5")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+//for page6 q6
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q6"]')
+let nextButton=document.getElementById("nextButton6")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+//for page7 q7
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let radioButtons=document.querySelectorAll('input[name="q7"]')
+let nextButton=document.getElementById("nextButton7")
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      nextButton.disabled = false;
+    });
+  });
+
+})
+//for page8 q8
+let ageselect=document.getElementById("ageselect");
+let nextButton=document.getElementById("nextButton8");
+ ageselect.addEventListener("change", ()=>{
+  if(ageselect.value !==""){
+    nextButton.disabled = false;
+  }else{
+    nextButton.disabled = true;
+  }
+ })
+
+ //for page9 q9
+ document.addEventListener("DOMContentLoaded", () => {
+
+ let nameEnter=document.getElementById("nameEnter")
+ let nextButton9=document.getElementById("nextButton9")
+
+   nameEnter.addEventListener("input", ()=>{
+    if(nameEnter.value.trim() !==""){
+      nextButton9.disabled =false;
+    }else{
+      nextButton9.disabled =true;
+    }
+   })
+  });
 
 
 
